48c377964d449111ada69a6c36af89993005591d
Ruben Garcia <rubengarciamohedano@gmail.com>
2019-03-22 14:28:35 +1100

    placeholder: unsubscribing

--- src/lambda/setup.js
+++ src/lambda/setup.js
@@ -3,7 +3,7 @@ const qs = require('querystring')
 const { TOKEN, BOT_URL } = process.env
 
 exports.handler = function (event, context, callback) {
-
+  // Adding a new webhook
   if (event.httpMethod == "POST"){
       console.log(event)
       let body = JSON.parse(event.body)
@@ -23,11 +23,40 @@ exports.handler = function (event, context, callback) {
           body: 'Project added'
         })
       }).catch(error => {
+        console.log(error.response.data.errors)
         callback(null, {
           statusCode: 200,
           body: 'Adding project failed'
         })
       })
+      return
+  }
+  // Deleting webhook
+  if (event.httpMethod == "DELETE"){
+      console.log(event)
+      let body = JSON.parse(event.body)
+      console.log(event)
+  //     let url = "https://app.asana.com/api/1.0/webhooks"
+  //     let content = {
+  //       target: BOT_URL,
+  //       resource: body.project
+  // }
+  //     axios.post(url, qs.stringify(content), {
+  //       headers: {
+  //         'Authorization': TOKEN
+  //       }
+  //     }).then(res => {
+  //       callback(null, {
+  //         statusCode: 200,
+  //         body: 'Project added'
+  //       })
+  //     }).catch(error => {
+  //       console.log(error.response.data.errors)
+  //       callback(null, {
+  //         statusCode: 200,
+  //         body: 'Adding project failed'
+  //       })
+  //     })
       return
   }
   callback(null, {
